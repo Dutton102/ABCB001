5c06b317ebb4655fee47b26aea96cc4398651bbd
Anders Burslund <andersburslund@hotmail.com>
2016-12-03 19:18:17 +0100

    Progress: swapStyleSheet function now applied to webpage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 // Script
 $(document).ready(function(){
 	var forside = $('body#forside')
-
-	if (forside.length) {
-		forside.append('<div id="style-selector"></div>');
+		function swapStyleSheet(sheet) {
+		document.getElementById('stylesheet').setAttribute('href', sheet);
 	}
 
+
+	if (forside.length) {
+		forside.prepend('<div id="style-selector">' +
+		 '<h2>Vælg style:</h2>' +
+		  '<button id="duttonknap">Dutton</button>' +
+		   '<button id="brassøeknap">Brassøe</button>' +
+		    '</div>');
+
+		$('#brassøeknap').click(function(){
+			swapStyleSheet('style/style.css');
+		});
+		$('#duttonknap').click(function(){
+			swapStyleSheet('style/dutton.css');
+		});
+	}
 });
